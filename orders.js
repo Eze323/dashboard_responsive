6ee2ea3f75693ce8372f220e025bdc2c7fae9bc2
ALBERTO EZEQUIEL IRACE <103865149+Eze323@users.noreply.github.com>
2024-05-10 00:38:34 -0300

    primer push

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders=[
+    {
+        productName:'Javascript Tutorial',
+        productNumber:'85631',
+        paymentStatus:'Due',
+        status:'Pending'
+    },
+    {
+        productName:'CSS Full Course',
+        productNumber:'97248',
+        paymentStatus:'Refunded',
+        status:'Declined'
+    },
+    {
+        productName:'Flex-Box Tutorial',
+        productNumber:'38456',
+        paymentStatus:'Paid',
+        status:'Active'
+    }
+]
